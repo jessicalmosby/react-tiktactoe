08ffe3d3ff9039bbcc4542ac9e370666b1ad1b3a
Jessica Mosby <jessicamosby@mac.com>
2023-01-26 15:17:08 -0500

    have box component rendering on page

--- src/Context/GameContext.js
+++ src/Context/GameContext.js
@@ -4,8 +4,26 @@ import boardData from '../boardData.js';
 const GameContext = createContext();
 const GameProvider = ({ children }) => {
   const [board, setBoard] = useState(boardData);
+  const [currentPlayer, setCurrentPlayer] = useState('x');
+  const [gameMessage, setGameMessage] = useState('');
+  const [active, setActive] = useState(true);
 
-  return <GameContext.Provider value={{ board, setBoard }}>{children}</GameContext.Provider>;
+  return (
+    <GameContext.Provider
+      value={{
+        board,
+        setBoard,
+        currentPlayer,
+        setCurrentPlayer,
+        gameMessage,
+        setGameMessage,
+        active,
+        setActive,
+      }}
+    >
+      {children}
+    </GameContext.Provider>
+  );
 };
 
 const useGameContext = () => {

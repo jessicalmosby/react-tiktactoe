ff1bc37d8267273c165d5fc0daa6f33051d959d6
Jessica Mosby <jessicamosby@mac.com>
2023-01-27 20:16:26 -0500

    have cats game working

--- src/Context/GameContext.js
+++ src/Context/GameContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext } from 'react';
+import { createContext, useState, useContext, useEffect } from 'react';
 import boardData from '../boardData.js';
 
 const GameContext = createContext();
@@ -14,6 +14,25 @@ const GameProvider = ({ children }) => {
       board[space] = { space: space, content: currentPlayer };
       setCurrentPlayer(currentPlayer === 'x' ? 'o' : 'x');
       checkWinner(board);
+      setBoard(board);
+    }
+  };
+
+  const boardContent = [];
+  for (let box of board) {
+    boardContent.push(box.content);
+  }
+
+  const checkGameStatus = (board) => {
+    if (!active) return;
+    const winner = checkWinner(board);
+    if (winner) {
+      setGameMessage(`You win ${winner}!`);
+      setActive(false);
+    } else if (!boardContent.some((i) => i === '')) {
+      console.log('Cats');
+      setGameMessage('Oop, Cats Game!');
+      setActive(false);
     }
   };
 
@@ -29,6 +48,7 @@ const GameProvider = ({ children }) => {
         active,
         setActive,
         handleClick,
+        checkGameStatus,
       }}
     >
       {children}
@@ -42,19 +62,21 @@ const checkMatch = (a, b, c) => {
 };
 
 const checkWinner = (board) => {
-  if (checkMatch(board[0].content, board[1].content, board[2].content)) console.log('winner');
-  if (checkMatch(board[3].content, board[4].content, board[5].content)) console.log('winner');
-  if (checkMatch(board[6].content, board[7].content, board[8].content)) console.log('winner');
-  if (checkMatch(board[0].content, board[3].content, board[6].content)) console.log('winner');
-  if (checkMatch(board[1].content, board[4].content, board[7].content)) console.log('winner');
-  if (checkMatch(board[1].content, board[4].content, board[7].content)) console.log('winner');
-  if (checkMatch(board[2].content, board[5].content, board[8].content)) console.log('winner');
-  if (checkMatch(board[0].content, board[4].content, board[8].content)) console.log('winner');
-  if (checkMatch(board[2].content, board[4].content, board[6].content)) console.log('winner');
+  if (checkMatch(board[0].content, board[1].content, board[2].content)) return board[0].content;
+  if (checkMatch(board[3].content, board[4].content, board[5].content)) return board[3].content;
+  if (checkMatch(board[6].content, board[7].content, board[8].content)) return board[6].content;
+  if (checkMatch(board[0].content, board[3].content, board[6].content)) return board[0].content;
+  if (checkMatch(board[1].content, board[4].content, board[7].content)) return board[1].content;
+  if (checkMatch(board[2].content, board[5].content, board[8].content)) return board[2].content;
+  if (checkMatch(board[0].content, board[4].content, board[8].content)) return board[0].content;
+  if (checkMatch(board[2].content, board[4].content, board[6].content)) return board[2].content;
 };
 
 const useGameContext = () => {
-  const gameContext = useContext(GameContext);
-  return gameContext;
+  const context = useContext(GameContext);
+  useEffect(() => {
+    context.checkGameStatus(context.board);
+  }, [context]);
+  return context;
 };
 export { GameProvider, useGameContext };

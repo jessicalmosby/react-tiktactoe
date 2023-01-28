cb4a084ba6ae6a2282fb846445fb477996a71c2d
Jessica Mosby <jessicamosby@mac.com>
2023-01-27 21:00:57 -0500

    added new game button

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,11 +2,29 @@ import React from 'react';
 import { useGameContext } from '../../Context/GameContext.js';
 
 export default function Header() {
-  const { gameMessage } = useGameContext();
+  const { gameMessage, setGameMessage, setBoard, setCurrentPlayer, setActive } = useGameContext();
+
+  function newGame() {
+    setActive(true);
+    setBoard([
+      { space: 0, content: '' },
+      { space: 1, content: '' },
+      { space: 2, content: '' },
+      { space: 3, content: '' },
+      { space: 4, content: '' },
+      { space: 5, content: '' },
+      { space: 6, content: '' },
+      { space: 7, content: '' },
+      { space: 8, content: '' },
+    ]);
+    setCurrentPlayer('x');
+    setGameMessage('');
+  }
 
   return (
     <div className="header">
       <p>{gameMessage}</p>
+      <button onClick={newGame}>New Game!</button>
     </div>
   );
 }
